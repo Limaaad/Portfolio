08b6cf4d277cc4ed61cf1a7cdd280400f0808997
Limaaad <fabielledelima@gmail.com>
2024-02-11 14:06:32 -0300

    aaaaaaaaaaaaaaaaa

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-const buttonMenu = document.querySelector('.menu-btn');
+$(document).ready(function() {
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
+    $(window).scroll(function() {
+
+        if (this.scrollY >= 20) {
+            $('.navbar').addClass('sticky');
         } else {
-            $('.navbar').removeClass("sticky");
+            $('.navbar').removeClass('sticky');
         }
     });
-});
-
-buttonMenu.addEventListener('click', () => {
-    if () {
-    
-    }
 });
