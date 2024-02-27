ae78730ce5f15b704d343a5b6054e7c5aa5ef212
Limaaad <fabielledelima@gmail.com>
2024-02-27 18:20:03 -0300

    ajust navbar

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 $(document).ready(function() {
-
-    $(window).scroll(function() {
-
-        if (this.scrollY > 20) {
-            $('.navbar').addClass('sticky');
+    $(window).scroll(function(){
+        if (this.scrollY > 20){
+            $('.navbar').addClass("sticky");
         } else {
-            $('.navbar').removeClass('sticky');
+            $('.navbar').removeClass("sticky");
         }
     });
-});
+})
 
-console.log (stiky);
+console.log('sticky')
 
